f06f26d94f96ffdd2567af770a35e0bb005f4e08
Mark64-1 <led02337@gmail.com>
2020-12-22 14:21:06 +0900

    3차 수정

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config/key')
+const {User} =  require("./models/User")
 
-mongoose.connect('mongodb+srv://user:1234@cluster0.zb7qp.mongodb.net/test?retryWrites=true&w=majority',{
+//application/x-www-form-urlencoded 아마..?
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(()=>console.log("MongoDB Connected...")).catch(err=>console.log(err));
 
 app.get('/',((req, res) => res.send('hello World!')));
+
+app.post('/register',((req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
+}))
+
 
 app.listen(port,()=>console.log('Example app listening on port ${port}!'));
