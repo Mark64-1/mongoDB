3cd1206eb50f068b0f6d326968550eaf6e236f46
Mark64-1 <led02337@gmail.com>
2020-12-23 09:52:17 +0900

    이메일과 비밀번호 검사를 동시에 함 + comparePassword 삭제

--- index.js
+++ index.js
@@ -42,18 +42,20 @@ app.post('/login',(req,res)=>{
                 message:"제공된 이메일에 해당하는 유저가 없습니다."
             })
         }
-
-        user.comparePassword(req.body.password,(err,isMatch)=>{
-            if(!isMatch)return res.json({loginSuccess:false,message:"비밀번호가 틀렸습니다."})
-            user.generateToken((err,user)=>{
-                if(err) return res.status(400).send(err);
-                res.cookie("x_auth",user.token)
-                    .status('200')
-                    .json({loginSuccess: true,userId: user._id})
-            })
-
-
+        User.findOne({password:req.body.password},(err,user)=>{
+            if(!user)return res.json({loginSuccess:false,message:"비밀번호가 틀렸습니다."})
+            res.json({message:"로그인에 성공했습니다."})
         })
+
+        // user.comparePassword(req.body.password,(err,isMatch)=>{
+        //     if(!isMatch)return res.json({loginSuccess:false,message:"비밀번호가 틀렸습니다."})
+        //     user.generateToken((err,user)=>{
+        //         if(err) return res.status(400).send(err);
+        //         res.cookie("x_auth",user.token)
+        //             .status('200')
+        //             .json({loginSuccess: true,userId: user._id})
+        //     })
+        // })
     })
 
 
